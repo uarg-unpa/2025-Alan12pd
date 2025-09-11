6e6f948992ec55e6259ca3e8872211ab6b49d275
Alan12pd <alandavidparada989@gmail.com>
2025-09-11 21:22:50 +0000

    Trabajo en grupo con Castillo christian de clase Empleado y Departamento

--- TP2/Departamento.java
+++ TP2/Departamento.java
@@ -1,35 +1,45 @@
 package TP2;
 
 public class Departamento {
-        private String nombre;
-       private int piso;
+    private String nombre;
+    private int piso;
 
-            public Departamento (String nombre, int piso){
-            if(nombre != null && !nombre.trim().isEmpty()){
+    public Departamento(String nombre, int piso) {
+        if (nombre != null && !nombre.trim().isEmpty()) {
             this.nombre = nombre;
-                    }
-    if (piso > 0){
-    this.piso = piso;
-           }
-         public void setNombre (String Nombre){
-         this.nombre = nombre;
-      } 
-      public String nombre (){
-            return this.nombre;
-            }
-             public void setPiso (int Piso){
-            this.piso = piso;
+        } else {
+            this.nombre = "Desconocido"; 
         }
-            public int piso (){
-             return this.piso;
-         }
-                                  public boolean esCompleto (){
-              if(!nombre.isEmpty() && piso > 0){
-                  return true;
-              }else{
-                  return false;
-              }
-              }
-              public String toString (){
-              return nombre + " " + piso;
+
+        if (piso > 0) {
+            this.piso = piso;
+        } else {
+            this.piso = 1; 
+        }
     }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public int getPiso() {
+        return this.piso;
+    }
+
+    public boolean esCompleto() {
+        return nombre != null && !nombre.isEmpty() && piso > 0;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + piso;
+    }
+}
--- TP2/Empleado.java
+++ TP2/Empleado.java
@@ -9,101 +9,144 @@ public class Empleado {
     private String apellido;
     private String cargo;
 
-
-
-    public empleado (int legajo, double sueldo, departamento departamento, string apellido, string cargo) {
-        this.legajo;
-        this.sueldo;
-        this.departamento;
-        this.apellido;
-        this.cargo;
+    public Empleado (int legajo, double sueldo, Departamento departamento, String apellido, String cargo){
+        this.legajo=legajo;
+        this.sueldo=sueldo;
+        this.departamento=departamento;
+        this.apellido=apellido;
+        this.cargo= cargo;
     }
-
-        public void setlegajo (int legajo){
-            this.legajo;
+        public void setLegajo ( int legajo){
+            this.legajo = legajo;
         }
-        public void getlegajo (int legajo){
+        public int getLegajo (){
             return this.legajo;
         }
-        public void setsueldo (double sueldo){
-            this.sueldo;
+        public void setSueldo (double sueldo){
+            this.sueldo = sueldo;
         }
-        public void getsueldo (double sueldo){
+        public double getSueldo (){
             return this.sueldo;
         }
-        public void setdepartamento (String departamento){
-            this.departamento;
+        public void setDepartamento (Departamento departamento){
+            this.departamento = departamento;
         }
-        public void getdepartamento (String departamento){
+        public Departamento getDepartamento (){
             return this.departamento;
         }
-        public void setapellido (String apellido){
-            this.apellido;
+        public void setApellido (String apellido){
+            this.apellido = apellido;
         }
-        public void getapellido (String apellido){
+        public String getApellido (){
             return this.apellido;
         }
-        public void setcargo (String Cargo){
-            this.Cargo;
+        public void setCargo (String cargo){
+            this.cargo = cargo;
         }
-        public void getcargo (String cargo){
-            return this.cargo
+        public String getCargo (){
+            return this.cargo;
         }
-        public esJefe (){
-            if(this.cargo == "Jefe"){
-                return true;
-            } else {
-            return false;
-            }
-             public void aumentarSueldo (double aumento){
-                if(aumento > 0 ){
-                this.sueldo = this.sueldo + aumento;
+
+            public boolean esJefe(){
+                if (this.cargo == "jefe"){
+                    return true;
+                }else{
+                    return false;
                 }
-                 public double calcularBono (double porcentaje){
-                    if (porcentaje > 0){
-                        return this.sueldo * (porcentaje);
-                    } else{
-                        return 0;
+            }
+                    public void aumentarSueldo (double aumento){
+                        if (aumento  > 0){
+                            this.sueldo= this.sueldo + aumento;
+                        }
                     }
-                 }
-                 public String toString (); {
-                    return "Empleado [legajo" + legajo +", sueldo" + sueldo + ", departamento" + departamento + ", apellido" + apellido + ", cargo" + cargo + "]"; 
-                 }
-             }
-             public static void main (String[] args) {
-                Departamento d1=new Departamento (e1, e2)
-                Departamento d2=new Departamento
+                        public double calcularBono (double porcentaje){
+                            if (porcentaje > 0){
+                                return this.sueldo * (porcentaje);
+                            } else {
+                                return 0;
+                            }
+                        }
+                            public String toString (){
+                                return "Empleado [legajo=" + legajo + ", sueldo=" + sueldo + ", departamento= " + departamento + ", apellido= " + apellido + ", cargo= " + cargo + "]";
 
-                  Scanner sc = new Scanner(System.in);
-                  System.out.println("ingrese legajo");
-                  int legajo = sc.nextInt();
-                  sc.nextInt();
+                            }
+                                public static void main (String arg[]){
+                                    Departamento d1= new Departamento ("RR HH", 3);
+                                    Departamento d2= new Departamento ("contabilidad", 2);
+                                    
+                                    Scanner sc = new Scanner (System.in);
 
-                   System.out.println("ingrese sueldo");
-                   double sueldo = sc.nextInt();
-                   sc.nextInt();
+                                    System.out.println("ingrese legajo");
+                                    int legajo = sc.nextInt ();
+                                    sc.nextLine();
 
-                   System.out.println("ingrese departamento");
-                   Departamento departamento = sc.nextLine();
-                   sc.nextLine();
+                                    System.out.println("ingrese sueldo");
+                                    double sueldo = sc.nextDouble();
+                                    sc.nextLine();
+                                    
+                                    System.out.println("ingrese departamento");
+                                    System.out.println("1 para RR HH, 2 para contabilidad");
+                                    Departamento departamento;
+                                    int dep = sc.nextInt();
+                                    if (dep== 1){
+                                        departamento = d1;
+                                    } else {
+                                        departamento = d2;
+                                    }
+                                    sc.nextLine();
 
-                   System.out.println("ingrese apellido");
-                   String apellido = sc.nextLine();
-                   sc.nextLine();
+                                    System.out.println("ingrese apellido");
+                                    String apellido = sc.nextLine();
+                                    sc.nextLine();  
 
-                   System.out.println("ingrese cargo");
-                   String cargo = sc.nextLine();
-                   sc.nextLine();
+                                    System.out.println("ingrese cargo");
+                                    String cargo = sc.nextLine();
+                                    sc.nextLine();
+                                    
+                                    Empleado e1= new Empleado (legajo, sueldo, departamento, apellido, cargo);
+                                    System.out.println(e1);
+                                    System.out.println("es jefe? " + e1.esJefe());
+                                    System.out.println("ingrese aumento");
+                                    double aumento = sc.nextDouble();
+                                    e1.aumentarSueldo(aumento);
+                                    System.out.println("su nuevo sueldo es " + e1.getSueldo());
+                                    System.out.println("ingrese porcentaje de bono");
+                                    double porcentaje = sc.nextDouble();
+                                    System.out.println("su bono es " + e1.calcularBono(porcentaje));
+                                    sc.nextLine();
 
-                   Empleado e1= new Empleado (legajo, sueldo, departamento, apellido, cargo);
-                   System.out.println()
+                                    System.out.println("ingrese legajo");
+                                    legajo = sc.nextInt ();
+                                    sc.nextLine();
 
+                                    System.out.println("ingrese sueldo");
+                                    sueldo = sc.nextDouble();
+                                    sc.nextLine();
+                                    
+                                    System.out.println("ingrese departamento");
+                                    System.err.println("1 para RR HH, 2 para contabilidad");
+                                    Departamento departamento2;
+                                    int depto = sc.nextInt();
+                                    if (depto== 1){
+                                        departamento2 = d1;
+                                    } else {
+                                        departamento2 = d2;
+                                    }
+                                    sc.nextLine();
 
-             }
+                                    System.out.println("ingrese apellido");
+                                    apellido = sc.nextLine();
+                                    sc.nextLine();  
 
+                                    System.out.println("ingrese cargo");
+                                    cargo = sc.nextLine();
+                                    sc.nextLine();
+                                    
+                                    Empleado e2 = new Empleado (legajo, sueldo, departamento2, apellido, cargo);
+                                    System.out.println(e2);
+                                    
+                                    sc.close();
 
-
-
-
+                                    
+                                }
     }
-}
